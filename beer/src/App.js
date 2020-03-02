6bfdf3f29317fa1a3b0ddf81137d9b6b261a46e9
Brad Donakowski <braddonako@gmail.com>
2020-03-02 11:32:32 -0700

    Created a simple navbar

--- beer/src/App.js
+++ beer/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home/home';
 import About from './components/About/about'
+import Header from './components/HeaderCompnent/header'
 
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <Header />
         <Switch>  
           <Route path="/" component={Home} exact/>
           <Route path='/about' component={About} exact/>
