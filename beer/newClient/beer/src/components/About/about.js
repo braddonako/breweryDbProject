a1d132caf23aee458292380f760bd7db36217edc
Brad Donakowski <braddonako@gmail.com>
2020-03-03 07:58:52 -0700

    Restarting react

---
+++ beer/newClient/beer/src/components/About/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is going to be an application using the breweryDB API. I know that me and many others, love beer. Users will be able to search breweries that are local to them(Or anywhere in the US of A), and see what kind of beers that they offer. I was originally going to create this App using strictly front end technologies, but I am going to store my api key on the backend. For the backend the technologies I am using are express.js and node.js</p>
+        </div>
+    );
+};
+
+export default About;
