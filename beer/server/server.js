8814ec211ff2e16df9837d1518ab0047bf6e1b36
Brad Donakowski <braddonako@gmail.com>
2020-03-02 23:07:50 -0700

    gitignore works

--- beer/server/server.js
+++ beer/server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fetch = require('node-fetch')
+const mongoose = require('mongoose')
 
 const app = express();
 require('dotenv').config();
