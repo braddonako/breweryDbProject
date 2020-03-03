8425c8f374094da58428d41e40de237b9fda685a
Brad Donakowski <braddonako@gmail.com>
2020-03-03 08:04:36 -0700

    added home component

---
+++ beer/newClient/beer/src/components/Home/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>This is the home page for my BreweryDB app</h1>
+            <p>Some cool stuff will go here yeee yeee</p>
+        </div>
+    );
+};
+
+export default Home;
