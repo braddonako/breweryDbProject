c1fc3be1189eb0e07a3085fbba9cd646f4cbabe8
Brad Donakowski <braddonako@gmail.com>
2020-03-03 08:58:08 -0700

    added beers page

---
+++ beer/newClient/beer/src/components/Beers/beers.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Beers extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Beers will go here yeee yee</h1>
+            </div>
+        );
+    }
+}
+
+export default Beers;
