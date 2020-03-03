9de950ce14137f45124eeb90f614a3fa4f57b61a
Brad Donakowski <braddonako@gmail.com>
2020-03-03 08:57:21 -0700

    created a very, very simple page for the breweries

---
+++ beer/newClient/beer/src/components/Breweries/breweries.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class breweries extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Heya - here are some breweries</h1>
+            </div>
+        );
+    }
+}
+
+export default breweries;
