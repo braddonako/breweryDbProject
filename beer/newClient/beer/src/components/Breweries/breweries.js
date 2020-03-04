1ff5cb2f8544a5e037489f68f168c5adcfbfff25
Brad Donakowski <braddonako@gmail.com>
2020-03-04 10:29:27 -0700

    actually received a response from the server, no data tho

--- beer/newClient/beer/src/components/Breweries/breweries.js
+++ beer/newClient/beer/src/components/Breweries/breweries.js
@@ -8,6 +8,30 @@ class Breweries extends Component {
         }
     }
 
+    componentDidMount() {
+        this.getAllBreweries();
+    }
+
+    getAllBreweries = async () =>{
+        try {
+            const allBreweries = await fetch(process.env.REACT_APP_BACKEND_URL + '/breweries', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                // body: JSON.stringify()
+            });
+            // const parsedAllBreweries = await allBreweries.json();
+            // console.log('Brewery data -->', parsedAllBreweries);
+            console.log(allBreweries)
+            // this.setState({
+            //     allBreweries: parsedAllBreweries.data
+            // })
+        } catch(err){
+            console.log(err)
+        }
+    }
+
     render() {
         return (
             <div>
