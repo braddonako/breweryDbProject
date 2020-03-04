bebf63657438075c06683f965a8628025e5dc2ce
Brad Donakowski <braddonako@gmail.com>
2020-03-03 20:10:22 -0700

    Added webpack

--- beer/newClient/new-beer/src/App.js
+++ beer/newClient/new-beer/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home/home';
-import About from './components/About/about';
+// import About from './components/About/about';
 import Header from './components/HeaderComponent/header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -12,7 +12,7 @@ function App() {
         <Header />
         <Switch>
           <Route path="/" component={Home} exact />
-          <Route path='/about' component={About} exact />
+          {/* <Route path='/about' component={About} exact /> */}
         </Switch>
       </div>
     </BrowserRouter>
