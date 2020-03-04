fbf3f626ddeeaf73c271b694dd3fe9c3aaebc1a5
Brad Donakowski <braddonako@gmail.com>
2020-03-03 17:20:07 -0700

    Server is now accepting requests on postman

--- beer/server/server.js
+++ beer/server/server.js
@@ -7,30 +7,43 @@ const app = express();
 require('dotenv').config();
 
 
-// mongoose.Promise = global.Promise;
-// mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 8000;
+
 const apiKey = process.env.API_KEY; 
+
+//-------------------------///
+        //MODELS//
+//-------------------------///
+
+const{Breweries} = require('./models/breweries')
+
+app.get('/', (req, res) => {
+    res.send('Your mama')
+})
+ 
 
 app.get('/breweries', async (req, res)=>{
     const apiUrl = `http://api.brewerydb.com/v2/breweries/?key=${apiKey}`;
     const response = await fetch(apiUrl);
     const json = await response.json();
+    res.send(json)
     console.log(json);
-
 })
 
 app.get('/beers', async(req,res) => {
     const apiUrl = `http://api.brewerydb.com/v2/beers/?key=${apiKey}`;
     const response = await fetch(apiUrl);
     const json = await response.json();
+    res.send(json)
     console.log(json);
 })
 
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
     console.log(`I am listening. ${port}`)
 })
