e1f38ff60708dd22db18650440105bd79008fb49
Brad Donakowski <braddonako@gmail.com>
2020-03-03 19:24:21 -0700

    Added comments

--- beer/newClient/beer/src/components/Breweries/breweries.js
+++ beer/newClient/beer/src/components/Breweries/breweries.js
@@ -1,32 +1,41 @@
 import React, { Component } from 'react';
-
-
+// import { parse } from 'url';
 
 class Breweries extends Component {
     constructor(props){
         super(props);
         this.state = {
-            breweries: [],
+            brewery: [],
         }
     }
+    componentDidMount(){
+       this.getAllBreweries()
+    }
 
-   getAllBreweries = async () => {
-       try {
-           const breweries = await fetch(`http://localhost:8000/breweries`);
-           const parsedBreweries = await breweries.json();
-           console.log(parsedBreweries);
-           this.setState({
-               breweries: parsedBreweries.data.name
-           })
-       } catch(err){
-           console.log(err)
-       }
-   }
+    getAllBreweries = async () => {
+        try{
+            const breweries = await fetch('http:localhost:8000/breweries');
+            const parsedBreweries = await breweries.json();
+            console.log(parsedBreweries);
+            this.setState({
+                brewery: parsedBreweries.data[0]
+            })
+        } catch(err){
+            console.log(err)
+        }
+    }
 
     render() {
         return (
             <div>
-                <h1>Heya - here are some breweries</h1>
+                <div>
+                    <h1>Heya - here are some breweries</h1>
+                </div>
+                <div className='breweries'>
+                    <ul>
+                        <li>{this.state.brewery}</li>
+                    </ul>
+                </div>
             </div>
         );
     }
--- beer/server/server.js
+++ beer/server/server.js
@@ -22,17 +22,22 @@ const apiKey = process.env.API_KEY;
 
 const{Breweries} = require('./models/breweries')
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { // testing out the initial route
     res.send('Your mama')
 })
  
-
+// grabbng all of the beers in breweryDB
 app.get('/breweries', async (req, res)=>{
+    // hiding apiKey in .env file. I am running off of the local server,
+    // but am retrieving the data from breweryDB
+
     const apiUrl = `http://api.brewerydb.com/v2/breweries/?key=${apiKey}`;
     const response = await fetch(apiUrl);
     const json = await response.json();
+    // turning the response from breweryDB data to JSON
     res.send(json)
     console.log(json);
+    // checking json in the terminal
 })
 
 app.get('/beers', async(req,res) => {
