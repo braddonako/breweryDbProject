e3ce66386cbd0e15fedb5af8d65e3938064af5b9
Brad Donakowski <braddonako@gmail.com>
2020-03-03 20:00:46 -0700

    Added react-router-dom and bootstrap

--- beer/newClient/new-beer/src/App.js
+++ beer/newClient/new-beer/src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './components/Home/home';
+import About from './components/About/about';
+import Header from './components/HeaderComponent/header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path='/about' component={About} exact />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
