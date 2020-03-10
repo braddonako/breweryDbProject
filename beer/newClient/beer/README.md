3a54bcb7d02c4dd56df9393987b5e310096d2561
Brad Donakowski <braddonako@gmail.com>
2020-03-09 21:16:15 -0600

    added footer content

--- beer/newClient/beer/README.md
+++ beer/newClient/beer/README.md
@@ -66,3 +66,10 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 ### `npm run build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+
+### NPM INSTALL
+fontawesome
+npm install --save @fortawesome/fontawesome-free
+
+npm install react-bootstrap bootstrap
+npm install react-router-dom
--- beer/newClient/beer/src/components/Header_Footer/Footer/footer.js
+++ beer/newClient/beer/src/components/Header_Footer/Footer/footer.js
@@ -1,10 +1,73 @@
 import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import faCompass from '@fortawesome/fontawesome-free-solid/faCompass';
+import faPhone from '@fortawesome/fontawesome-free-solid/faPhone';
+import faClock from '@fortawesome/fontawesome-free-solid/faClock';
+import faEnvelope from '@fortawesome/fontawesome-free-solid/faEnvelope';
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <footer>
+            <div className='container'>
+                <div className='logo'>
+                    Waves
+                </div>
+                <div className='wrapper'>
+                    <div className='left'>
+                        <h2>Contact Information</h2>
+                        <div className='business_nfo'>
+                            <div className='tag'>
+                                <FontAwesomeIcon 
+                                icon={faCompass}
+                                className='icon'
+                                />
+                                <div className='nfo'>
+                                    <div>Address</div>
+                                    <div>2172 S Trenton Way</div>
+                                </div>
+                            </div>
+                        <div className='tag'>
+                                <FontAwesomeIcon 
+                                icon={faPhone}
+                                className='icon'
+                                />
+                                <div className='nfo'>
+                                    <div>Phone</div>
+                                    <div>123456789</div>
+                                </div>
+                            </div>
+                        <div className='tag'>
+                                <FontAwesomeIcon 
+                                icon={faClock}
+                                className='icon'
+                                />
+                                <div className='nfo'>
+                                    <div>Working Hours</div>
+                                    <div>9am-5pm</div>
+                                </div>
+                            </div>
+                        <div className='tag'>
+                                <FontAwesomeIcon 
+                                icon={faEnvelope}
+                                className='icon'
+                                />
+                                <div className='nfo'>
+                                    <div>Email</div>
+                                    <div>braddonakowski@yahoo.com</div>
+                                </div>
+                        </div>
+                        </div>                      
+                    </div>
+                    <div className='left'>
+                        <h2>Be the first to know</h2>
+                        <div>
+                            <div>Hit us up for updates and what not! We love customers</div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </footer>
     );
 };
 
